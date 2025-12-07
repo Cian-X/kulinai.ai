8e4a4b216cbf353b523b3555b59fd956ac45efde
Aslam Muzaky <zakyaaslam2004@gmail.com>
2025-12-07 18:50:19 +0700

    fix serveStatic error & production port

--- server/index.js
+++ server/index.js
@@ -58,7 +58,11 @@ app.use((req, res) => {
   res.status(404).send('Cannot ' + req.method + ' ' + req.path);
 });
 if (process.env.NODE_ENV === "production") {
-  serveStatic(app);
+  //serveStatic(app);
 }
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on ${port}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
